0285e358284f19f360718b77a98a32c1a1a79eff
José Luis Rojo Sánchez <jose@artegrafico.net>
2019-06-27 14:33:32 +0200

    updated uninstall file

--- uninstall.php
+++ uninstall.php
@@ -3,13 +3,14 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) { die; }
 
 // borrar opciones
-delete_option( 'artegrafico-privacidad');
+delete_option( 'aglopdgdd_content_version');
+delete_option( 'aglopdgdd_content_settings');
  
 // for site options in Multisite
 //delete_site_option($option_name);
  
 // drop a custom database table
-global $wpdb;
+/*global $wpdb;
 $table = $wpdb->prefix . 'artegrafico_privacidad';
 $sql = 'DROP TABLE IF EXISTS '.$table;  
-$wpdb->query($sql);
+$wpdb->query($sql);*/
